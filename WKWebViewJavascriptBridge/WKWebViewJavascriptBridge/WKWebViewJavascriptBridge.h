a3eefe5d6835f4e0f9915d65361a4f625ed589b0
Lision <lisionmail@gmail.com>
2018-01-21 00:17:20 +0800

    add the project for WKWebViewJavascriptBridge

---
+++ WKWebViewJavascriptBridge/WKWebViewJavascriptBridge/WKWebViewJavascriptBridge.h
@@ -0,0 +1,19 @@
+//
+//  WKWebViewJavascriptBridge.h
+//  WKWebViewJavascriptBridge
+//
+//  Created by Lision on 2018/1/20.
+//  Copyright © 2018年 Lision. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WKWebViewJavascriptBridge.
+FOUNDATION_EXPORT double WKWebViewJavascriptBridgeVersionNumber;
+
+//! Project version string for WKWebViewJavascriptBridge.
+FOUNDATION_EXPORT const unsigned char WKWebViewJavascriptBridgeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WKWebViewJavascriptBridge/PublicHeader.h>
+
+
---
+++ WKWebViewJavascriptBridge/WKWebViewJavascriptBridgeTests/WKWebViewJavascriptBridgeTests.swift
@@ -0,0 +1,36 @@
+//
+//  WKWebViewJavascriptBridgeTests.swift
+//  WKWebViewJavascriptBridgeTests
+//
+//  Created by Lision on 2018/1/20.
+//  Copyright © 2018年 Lision. All rights reserved.
+//
+
+import XCTest
+@testable import WKWebViewJavascriptBridge
+
+class WKWebViewJavascriptBridgeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
