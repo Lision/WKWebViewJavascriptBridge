1887b581755a7a41e8cc85279ea6a59f4ae764e1
Lision <lisionmail@gmail.com>
2018-01-21 12:49:21 +0800

    Modify the file structure

---
+++ WKWebViewJavascriptBridge/WKWebViewJavascriptBridge.swift
@@ -0,0 +1,9 @@
+//
+//  WKWebViewJavascriptBridge.swift
+//  WKWebViewJavascriptBridge
+//
+//  Created by Lision on 2018/1/21.
+//  Copyright © 2018年 Lision. All rights reserved.
+//
+
+import Foundation
---
+++ WKWebViewJavascriptBridge/WKWebViewJavascriptBridgeBase.swift
@@ -0,0 +1,9 @@
+//
+//  WKWebViewJavascriptBridgeBase.swift
+//  WKWebViewJavascriptBridge
+//
+//  Created by Lision on 2018/1/21.
+//  Copyright © 2018年 Lision. All rights reserved.
+//
+
+import Foundation
