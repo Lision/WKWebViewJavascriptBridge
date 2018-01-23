d3308b7fb8b73ca50e451fda84d70f7fab7b9f69
Lision <lisionmail@gmail.com>
2018-01-23 20:47:41 +0800

    add demo and demo logic

---
+++ WKWebViewJavascriptBridgeDemo/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  WKWebViewJavascriptBridgeDemo
+//
+//  Created by Lision on 2018/1/23.
+//  Copyright © 2018年 Lision. All rights reserved.
+//
+
+import UIKit
+import WebKit
+import WKWebViewJavascriptBridge
+
+class ViewController: UIViewController {
+    let webView = WKWebView(frame: CGRect(), configuration: WKWebViewConfiguration())
+    var bridge: WKWebViewJavascriptBridge!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        webView.frame = view.bounds
+        webView.navigationDelegate = self
+        bridge = WKWebViewJavascriptBridge(webView: webView)
+        view.addSubview(webView)
+    }
+}
+
+extension ViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        print("webViewDidStartLoad")
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        print("webViewDidFinishLoad")
+    }
+}
