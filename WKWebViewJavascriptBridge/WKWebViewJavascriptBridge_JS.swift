1100903b6c5c5dd1489c2b4fa63cb49bd5c934a5
Lision <lisionmail@gmail.com>
2018-01-23 21:03:41 +0800

    add WKWebViewJavascriptBridgeJS

--- WKWebViewJavascriptBridge/WKWebViewJavascriptBridge_JS.swift
+++
@@ -1,9 +0,0 @@
-//
-//  WKWebViewJavascriptBridge_JS.swift
-//  WKWebViewJavascriptBridge
-//
-//  Created by Lision on 2018/1/21.
-//  Copyright © 2018年 Lision. All rights reserved.
-//
-
-import Foundation
