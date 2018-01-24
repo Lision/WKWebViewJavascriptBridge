694b03f57171415b97b8bdb030509079c21e78f7
Lision <lisionmail@gmail.com>
2018-01-24 13:45:05 +0800

    fix demo code

--- WKWebViewJavascriptBridgeDemo/ViewController.swift
+++ WKWebViewJavascriptBridgeDemo/ViewController.swift
@@ -20,10 +20,27 @@ class ViewController: UIViewController {
         webView.navigationDelegate = self
         bridge = WKWebViewJavascriptBridge(webView: webView)
         view.addSubview(webView)
+        loadDemoPage()
     }
     
     func loadDemoPage() {
-        let pagePath = Bundle.main.path(forResource: "Demo", ofType: "html")
+        enum LoadDemoPageError: Error {
+            case nilPath
+        }
+        
+        do {
+            let pagePath = Bundle.main.path(forResource: "Demo", ofType: "html")
+            guard pagePath != nil else {
+                throw LoadDemoPageError.nilPath
+            }
+            let pageHtml = try String(contentsOfFile: pagePath!, encoding: .utf8)
+            let baseURL = URL(fileURLWithPath: pagePath!)
+            webView.loadHTMLString(pageHtml, baseURL: baseURL)
+        } catch LoadDemoPageError.nilPath {
+            print(print("webView loadDemoPage error: pagePath is nil"))
+        } catch let error {
+            print("webView loadDemoPage error: \(error)")
+        }
     }
 }
 
