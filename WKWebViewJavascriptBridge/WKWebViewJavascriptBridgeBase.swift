cb2feedbf60031bf74c716ff6039e8dbdb634cb2
Kevin VACQUIER <kevin@tutteo.com>
2022-02-14 16:43:52 +0100

    Update WKWebViewJavascriptBridgeBase.swift
    Migration from [String: Any] to Any

--- WKWebViewJavascriptBridge/WKWebViewJavascriptBridgeBase.swift
+++ WKWebViewJavascriptBridge/WKWebViewJavascriptBridgeBase.swift
@@ -25,7 +25,7 @@ public class WKWebViewJavascriptBridgeBase: NSObject {
     var isLogEnable = false
     
     public typealias Callback = (_ responseData: Any?) -> Void
-    public typealias Handler = (_ parameters: [String: Any]?, _ callback: Callback?) -> Void
+    public typealias Handler = (_ parameters: Any?, _ callback: Callback?) -> Void
     public typealias Message = [String: Any]
     
     weak var delegate: WKWebViewJavascriptBridgeBaseDelegate?
@@ -89,7 +89,7 @@ public class WKWebViewJavascriptBridgeBase: NSObject {
                     log("NoHandlerException, No handler for message from JS: \(message)")
                     continue
                 }
-                handler(message["data"] as? [String : Any], callback)
+                handler(message["data"] as Any, callback)
             }
         }
     }
